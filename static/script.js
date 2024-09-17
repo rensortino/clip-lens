70d728a9c05da3116370779f54f4854da9ffba97
Renato Sortino <renato.sortino@cea.fr>
2024-09-17 16:57:10 +0200

    Updated script

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
-let highlightTraces = [];
+let highlightColor = 'green';
 let originalColors = []
+let scatterSelected = -1;
 
 
 function setupHeatmapHovering(heatmapElement) {
@@ -20,17 +21,60 @@ function setupHeatmapHovering(heatmapElement) {
 }
 
 function setupScatterHovering(scatterElement) {
+    if (originalColors.length === 0){
+        originalColors = [...scatterElement.data[0].marker.color];
+    }
+
     scatterElement.on('plotly_hover', function(data) {
         const point = data.points[0];
         const index = point.pointNumber;
-        Plotly.Fx.hover("heatmap", [
-            {pointNumber: [index, index]}
-        ])
-        updateImages(index, index)
+        if (scatterSelected === -1) {
+            Plotly.Fx.hover("heatmap", [
+                {pointNumber: [index, index]}
+            ])
+            updateImages(index, index)
+        } else {
+            Plotly.Fx.hover("heatmap", [
+                {pointNumber: [scatterSelected, index]}
+            ])
+            updateImages(scatterSelected, index)
+        }
     });
+
+    scatterElement.on('plotly_click', function(data) {
+        const point = data.points[0];
+        const index = point.pointNumber;
+
+        var pn='',
+            tn='';
+        for(var i=0; i < data.points.length; i++){
+            pn = data.points[i].pointNumber;
+            tn = data.points[i].curveNumber;
+            colors = data.points[i].data.marker.color;
+        };
+
+        if (scatterSelected !== -1)
+            colors[scatterSelected] = originalColors[scatterSelected]
+
+        if (index === scatterSelected) {
+            scatterSelected = -1
+            colors[pn] = originalColors[pn];
+        } else {
+            scatterSelected = index
+            colors[pn] = highlightColor;
+        }
+
+        var update = {'marker':{color: colors, size: 12}};
+        Plotly.restyle(scatterElement.id, update, [tn]);
+    });
+
     scatterElement.on('plotly_unhover', function(data) {
-        document.getElementById('image1').src = "";
-        document.getElementById('image2').src = "";
+        if (scatterSelected === -1) {
+            document.getElementById('image1').src = "";
+            document.getElementById('image2').src = "";
+        } else {
+            document.getElementById('image2').src = "";
+        }
     });
 }
 
@@ -40,5 +84,7 @@ function updateImages(row, col) {
         .then(json => {
             document.getElementById('image1').src = json.image1;
             document.getElementById('image2').src = json.image2;
+            document.getElementById('caption1').innerText = json.caption1;
+            document.getElementById('caption2').innerText = json.caption2;
         });
 }
